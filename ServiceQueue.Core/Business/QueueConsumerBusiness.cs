07aebc177bb9df6e99247d2e69f23414b19a397b
rubenlr <rubenlr@gmail.com>
2014-03-02 16:23:03 -0300

    Inicial

---
+++ ServiceQueue.Core/Business/QueueConsumerBusiness.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceQueue.Core.Model.Entity;
+
+namespace ServiceQueue.Core.Business
+{
+    class QueueConsumerBusiness
+    {
+        public QueueConsumerBusiness()
+        {
+        }
+
+        BlockingCollection<QueueTipoHandler> Queue { get; set; }
+
+        class QueueTipoHandler : QueueTipo
+        {
+            public QueueTipoHandler()
+            {
+                Pendente = new List<QueueItem>(100);
+                EmExecucao = new List<QueueItem>(100);
+            }
+
+            public ICollection<QueueItem> Pendente { get; private set; }
+            public ICollection<QueueItem> EmExecucao { get; private set; }
+        }
+
+        private void ManterEmExecucao(QueueTipoHandler tipo)
+        {
+            while (tipo.Pendente.Count > 0 && tipo.MaximoExecucoesSimultaneas > tipo.EmExecucao.Count)
+            {
+                QueueItem item;
+
+                lock (tipo)
+                    item = tipo.Pendente.OrderBy(x => x.Gravado).FirstOrDefault();
+
+                if (item != null)
+                {
+                    lock (tipo)
+                    {
+                        tipo.Pendente.Remove(item);
+                        tipo.EmExecucao.Add(item);
+                    }
+
+                    new Task(() =>
+                    {
+                        Executar(item);
+
+                        lock (tipo)
+                            tipo.EmExecucao.Remove(item);
+                    }).Start();
+                }
+            }
+        }
+
+        private void Executar(QueueItem item)
+        {
+
+        }
+    }
+}
---
+++ ServiceQueue.Core/Model/Entity/QueueItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace ServiceQueue.Core.Model.Entity
+{
+    class QueueItem
+    {
+        public QueueItem()
+        {
+            HistoricoExecucoes = new BlockingCollection<QueueHistoricoExecucao>();
+        }
+
+        public Guid Id { get; set; }
+        public Guid IdQueueTipo { get; set; }
+        public string Message { get; set; }
+        public string Lote { get; set; }
+        public DateTime Gravado { get; set; }
+        public DateTime Executado { get; set; }
+
+        public BlockingCollection<QueueHistoricoExecucao> HistoricoExecucoes { get; private set; }
+    }
+
+    class QueueHistoricoExecucao
+    {
+        public Guid Id { get; set; }
+        public Guid IdQueueItem { get; set; }
+        public DateTime Execucao { get; set; }
+        public bool Resultado { get; set; }
+        public string Retorno { set; get; }
+    }
+
+    class QueueTipo
+    {
+
+        public Guid Id { get; set; }
+        public int MaximoExecucoesSimultaneas { get; set; }
+        public string Url { get; set; }
+    }
+}
